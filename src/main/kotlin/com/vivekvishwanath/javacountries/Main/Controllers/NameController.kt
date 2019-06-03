eb2972751420946504d778af5f28a9275ea7ec81
VivekV95 <vivekv210@gmail.com>
2019-06-03 18:25:03 -0500

    Added Controller for /population endpoints

--- src/main/kotlin/com/vivekvishwanath/javacountries/Main/Controllers/NameController.kt
+++ src/main/kotlin/com/vivekvishwanath/javacountries/Main/Controllers/NameController.kt
@@ -4,13 +4,14 @@ import com.vivekvishwanath.javacountries.Main.JavaCountriesApplication
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/names")
 class NameController {
-    //localhost:2019/names/all
+
     @GetMapping(value = ["/all"], produces = ["application/json"])
     fun getAllNames(): ResponseEntity<Any> {
         val names = ArrayList<String>()
@@ -21,4 +22,21 @@ class NameController {
         }
         return ResponseEntity(names, HttpStatus.OK)
     }
+
+    @GetMapping(value = ["/start/{s}"], produces = ["application/json"])
+    fun getCountryByName(@PathVariable s: String): ResponseEntity<Any> {
+        val tempCountries = JavaCountriesApplication.myCountryList.countryList.
+                filter {it.name.startsWith(s, true)}
+        return ResponseEntity(tempCountries, HttpStatus.OK)
+    }
+
+    @GetMapping(value = ["/size/{length}"], produces = ["application/json"])
+    fun getCountriesByNameLength(@PathVariable length: Int): ResponseEntity<Any> {
+        val tempCountries = JavaCountriesApplication.myCountryList.countryList.
+                filter {it.name.length >= length}
+        return ResponseEntity(tempCountries, HttpStatus.OK)
+    }
+
+
+
 }
