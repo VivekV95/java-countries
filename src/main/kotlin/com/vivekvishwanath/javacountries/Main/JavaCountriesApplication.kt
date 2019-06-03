19ae62417395765856462153261c2c6f6dc16c8b
VivekV95 <vivekv210@gmail.com>
2019-06-03 14:32:55 -0500

    Created Country data class

--- src/main/kotlin/com/vivekvishwanath/javacountries/Main/JavaCountriesApplication.kt
+++ src/main/kotlin/com/vivekvishwanath/javacountries/Main/JavaCountriesApplication.kt
@@ -1,4 +1,4 @@
-package com.vivekvishwanath.javacountries
+package com.vivekvishwanath.javacountries.Main
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
