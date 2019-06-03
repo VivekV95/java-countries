63efe5e58a90cedb76bdadc66108ddb6a52a93d7
VivekV95 <vivekv210@gmail.com>
2019-06-03 14:17:31 -0500

    Initial commit

---
+++ src/main/kotlin/com/vivekvishwanath/javacountries/JavaCountriesApplication.kt
@@ -0,0 +1,11 @@
+package com.vivekvishwanath.javacountries
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaCountriesApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaCountriesApplication>(*args)
+}
